0025080d524f1864e1650a31cdb15eb4a86e6b20
Gerald Ng Qi Long <ngqilong@gmail.com>
2023-05-03 14:42:53 +0800

    first commit

---
+++ server/src/main/java/ibf2022/csf/workshop/day37/ws/Day37WsApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.csf.workshop.day37.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day37WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day37WsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/ibf2022/csf/workshop/day37/ws/Day37WsApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.csf.workshop.day37.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
